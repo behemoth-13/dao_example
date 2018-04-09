49111be6eaab3ee67bf359188e707a3762c2fc37
Alexei Afanasyeu <behemoth-13@yandex.ru>
2018-04-09 23:39:10 +0300

    Update DaoException.java

--- src/main/java/by/training/dao/DaoException.java
+++ src/main/java/by/training/dao/DaoException.java
@@ -1,8 +1,5 @@
 package by.training.dao;
 
-/**
- * Created by Afanasyeu Alexei on 09.04.2018.
- */
 public class DaoException extends Exception{
     public DaoException(Throwable cause) {
         super(cause);
